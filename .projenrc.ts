bf164ecf59987031481e231af3fd30ef9ea837ee
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2026-01-15 10:45:54 +0100

    fix!: Prevent importing entire CDK in consumers
    Added explicit exports for construct and runtime module. Before,
    using the runtime function would bundle the entire AWS CDK (55MB+) in
    your runtime code.
    
    BREAKING CHANGE: Imports now require an explicit path:
    
    Old:
    import { ConfigTable } from '@gemeentenijmegen/aws-config';
    
    New:
    import { ConfigTable } from '@gemeentenijmegen/aws-config/construct';
    
    Old:
    import { Config } from '@gemeentenijmegen/aws-config';
    
    New:
    import { Config } from '@gemeentenijmegen/aws-config/config';

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,11 +7,12 @@ const project = new GemeenteNijmegenCdkLib({
   cdkVersion: '2.1.0',
   devDeps: [
     '@gemeentenijmegen/projen-project-type',
+    'aws-cdk-lib',
   ],
   bundledDeps: [
-    '@gemeentenijmegen/utils',
     '@types/aws-lambda',
     '@aws-sdk/client-dynamodb',
+    '@gemeentenijmegen/utils',
   ],
   peerDeps: [
     'aws-cdk-lib',
@@ -25,7 +26,29 @@ const project = new GemeenteNijmegenCdkLib({
       'cdk.out',
     ],
   },
+  tsconfig: {
+    compilerOptions: {
+      isolatedModules: true,
+    },
+  },
+
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+// To prevent the runtime import from importing the entire CDK, we explicitly export 
+// the config or the cdk construct
+project.package.addField('exports', {
+  './construct': {
+    types: './lib/Construct/ConfigTableConstruct.d.ts',
+    import: './lib/Construct/ConfigTableConstruct.js',
+    require: './lib/Construct/ConfigTableConstruct.js',
+  },
+  './config': {
+    types: './lib/Config.d.ts',
+    import: './lib/Config.js',
+    require: './lib/Config.js',
+  },
+  './package.json': './package.json',
+});
+
 project.synth();
